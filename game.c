28a8a7249dd0b1f92208623bfa6444ece63c6573
root <root@DESKTOP-V35LFH8.localdomain>
2026-05-11 01:02:03 +0200

    create_room

---
+++ game.c
@@ -0,0 +1,22 @@
+room *create_room(int room_id){
+    room *salle = calloc(1,sizeof(room));
+    switch (room_id){
+        case 1:
+            salle->floor_level = 500;
+            salle->level = 1;
+        case 2:
+            salle->floor_level = 600;
+            salle->level = 2;
+        case 3:
+            salle->floor_level = 700;
+            salle->level = 1;
+        case 4:
+            salle->floor_level = 400;
+            salle->level = 1;
+        default:
+            salle ->floor_level = 100;
+            salle->level = 0;
+
+    }
+    return salle;
+}
